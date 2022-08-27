c07b71acef32ff096b49faadcb6f94410113405c
DevSaalih <soliualley@gmail.com>
2022-08-27 21:41:17 +0100

    Cors Cleared

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ require("dotenv").config();
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 const routes = require("./routes/bm_routes");
-
+const cors = require("cors");
 connectDB();
 const app = express();
 
-app.use(express.json({}));
+app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/lecture", routes);
